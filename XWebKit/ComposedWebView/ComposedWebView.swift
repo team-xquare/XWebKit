dd76f7d6cc420b63873a1bb5d0ffa4b796b4469d
Martin <kimxwan0319@naver.com>
2023-03-21 22:04:41 +0900

    ⚡️ :: 웹 리프레시시에 토큰 교체

--- XWebKit/ComposedWebView/ComposedWebView.swift
+++ XWebKit/ComposedWebView/ComposedWebView.swift
@@ -205,6 +205,14 @@ extension ComposedWebView {
         Task {
             let refresh = self.refreshControl.rx.controlEvent(.valueChanged).values
             for try await _ in refresh {
+                await webView.configuration.websiteDataStore.httpCookieStore.setCookie(HTTPCookie(properties: [
+                    .domain: ".xquare.app",
+                    .path: "/",
+                    .name: "accessToken",
+                    .value: self.state.accessTokenGetter(),
+                    .secure: "TRUE",
+                    HTTPCookiePropertyKey("HttpOnly"): true
+                ])!)
                 webView.reload()
             }
         }
